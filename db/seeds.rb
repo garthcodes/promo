80fd705d1568f7cb691fb383b938c3a5cccc14f8
Garth Gaughan <garth0323@gmail.com>
2018-03-20 19:49:16 -0700

    Create users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# fake users
+User.where(username: 'Holden').first_or_create
+User.where(username: 'Woody').first_or_create
+User.where(username: 'Roxy').first_or_create
+User.where(username: 'Jeter').first_or_create
